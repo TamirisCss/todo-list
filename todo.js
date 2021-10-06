b31b17806564c27a06ab23e43694626693e34d24
Tamiris Cristine <tamiris.css@gmail.com>
2021-10-06 11:41:12 +0200

    get date and time

--- todo.js
+++ todo.js
@@ -1,5 +1,8 @@
 const today = new Date();
 
+const weekDay = today.toLocaleString("default", { weekday: "long" }); 
+console.log(weekDay);
+
 const month = today.toLocaleString('default', { month: 'short' });
 const day = today.getDate();
 const year = today.getFullYear();
@@ -7,5 +10,7 @@ const year = today.getFullYear();
 const hour = today.getHours();
 const minutes = today.getMinutes();
 
-const date = `${month} ${day}, ${year}`;
-const time =  `${hour}:${minutes}`;
+const date = `${month} ${day}, ${year}`; 
+const time =  `${hour}:${minutes}`;    
+console.log(date);
+console.log(time);
