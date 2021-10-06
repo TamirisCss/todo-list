2c0ecf0898d988025370cb567e6e0ff83585a193
Tamiris Cristine <tamiris.css@gmail.com>
2021-10-06 11:26:00 +0200

    first commit

---
+++ todo.js
@@ -0,0 +1,11 @@
+const today = new Date();
+
+const month = today.toLocaleString('default', { month: 'short' });
+const day = today.getDate();
+const year = today.getFullYear();
+
+const hour = today.getHours();
+const minutes = today.getMinutes();
+
+const date = `${month} ${day}, ${year}`;
+const time =  `${hour}:${minutes}`;
