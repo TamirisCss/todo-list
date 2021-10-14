173b6758e0217e8db90dd04642b9b8c6be55f298
Tamiris Cristine <tamiris.css@gmail.com>
2021-10-14 17:47:27 +0200

    save todos to localStorage

--- todo.js
+++ todo.js
@@ -23,10 +23,23 @@ currentTime.innerHTML = time;
 //
 const todoInput = document.querySelector(".todo-input");
 const todoBtn = document.querySelector(".todo-button");
-const todoList = document.querySelector(".todo-list");
+const todoListUl = document.querySelector(".todo-list-ul");
 
 //EVENT LISTENERS
 todoBtn.addEventListener("click", addTodo);
+
+//criar um array pra guardar cada todo e se esta completo ou nao (boolean)
+//no addTodo so adicionar no array
+//criar um metodo que pega tudo que esta no array e mostra na tela (cria o html)
+//chamar esse metodo que atualiza a tela ao abrir a pagina e dentro do metodo addTodo
+//adicionar event no botao remover que acha e remove o item do array (e atualizar a tela)
+let todoList = window.localStorage.getItem("todos");
+let todos;
+if(todoList === null) {
+    todos = [];
+}else {
+    todos = JSON.parse(todoList);
+}
 
 //ADDING NEW TO DO
 function addTodo(event) {
@@ -42,12 +55,12 @@ function addTodo(event) {
     //todo li
     const newTodoLi = document.createElement("li");
     newTodoLi.classList.add("todo-item");
-    //dots icon
+    //trash icon
     const trashIcon = document.createElement("a");
     trashIcon.innerHTML = '<i class="fas fa-trash"></i>';
     trashIcon.classList.add("trash-icon");
 
-    todoList.appendChild(todoDiv);
+    todoListUl.appendChild(todoDiv);
     todoDiv.appendChild(checkbox);
     todoDiv.appendChild(newTodoLi);
     todoDiv.appendChild(trashIcon);
